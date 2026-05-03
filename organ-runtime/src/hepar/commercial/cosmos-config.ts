f4a2a19ba0ac1cec4fd5e54e20eab632cc73e7a8
zarasus37 <criscolon3@yahoo.com>
2026-05-03 08:27:31 -0500

    Use COSMOS_DB_NAME env var

--- organ-runtime/src/hepar/commercial/cosmos-config.ts
+++ organ-runtime/src/hepar/commercial/cosmos-config.ts
@@ -4,7 +4,7 @@ const endpoint = process.env.COSMOS_ENDPOINT || "https://localhost:8081";
 const key = process.env.COSMOS_KEY || "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
 const client = new CosmosClient({ endpoint, key });
-const databaseId = "hepar_commercial";
+const databaseId = process.env.COSMOS_DB_NAME || "hepar";
 
 export async function getContainer(containerId: string): Promise<Container> {
     const { database } = await client.databases.createIfNotExists({ id: databaseId });
